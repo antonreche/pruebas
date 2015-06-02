6040ac11832dc4b9880be83df4cf120eb084e0ba
antonreche <antonreche@gmail.com>
2015-06-03 01:19:20 +0200

    commit 1

---
+++ src/pruebas/Prueba.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package pruebas;
+
+/**
+ * @author Antonio Reche Ramal
+ */
+public class Prueba {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
